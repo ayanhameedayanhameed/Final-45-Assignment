fc5658ac443cb9db7d23eeecae735f700dffbb8d
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-05 00:59:06 +0500

    adding some one more file

---
+++ assignment no 06/main.js
@@ -0,0 +1,13 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Adition
+var additionResult = 4 + 4;
+console.log("Addition Result: ", additionResult);
+//Subtraction
+var subtractionResult = 12 - 4;
+console.log("Subtraction Result: ", subtractionResult);
+//Multiplication
+var multiplicationResult = 2 * 4;
+console.log("Multiplication Result: ", multiplicationResult);
+//Division
+var divisionResult = 64 / 8;
+console.log("Division Result: ", divisionResult);
---
+++ assignment no 06/main.ts
@@ -0,0 +1,24 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Adition
+
+const additionResult = 4 + 4
+
+console.log("Addition Result: ",additionResult)
+
+//Subtraction
+
+const subtractionResult = 12 - 4 
+
+console.log("Subtraction Result: ",subtractionResult)
+
+//Multiplication
+
+const multiplicationResult = 2 * 4
+
+console.log("Multiplication Result: ",multiplicationResult)
+
+//Division
+
+const divisionResult = 64 / 8
+
+console.log("Division Result: ",divisionResult)
