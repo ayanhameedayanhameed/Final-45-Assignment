6968c097b5ed9ffba11080865219a843076aaeab
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-05 02:29:09 +0500

    adding assignment no 08

---
+++ assignment no 08/main.js
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var FavoriteNumber = 2;
+var message = "My favorite number is ".concat(FavoriteNumber);
+console.log(message);
---
+++ assignment no 08/main.ts
@@ -0,0 +1,7 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let FavoriteNumber:number = 2
+
+let message: string = `My favorite number is ${FavoriteNumber}`
+
+console.log(message)
