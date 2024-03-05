01e6d35c34aa2634688701f98df6677e598b6495
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-05 11:11:36 +0500

    adding assignment 09

---
+++ assignment no 09/main.ts
@@ -0,0 +1,14 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//single line comment:
+
+//let personName = "Ayan";
+
+//Multi line comments:
+
+/*console.log("Lowercase:", personName.toLowerCase());
+
+console.log("Uppercase:", personName.toUpperCase());
+
+console.log("Titlecase:", personName.replace(/\b\w/g, c => c.toUpperCase()));
+*/
