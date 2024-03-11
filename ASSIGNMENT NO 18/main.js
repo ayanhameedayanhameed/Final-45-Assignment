e981dca39c2af04dbfe19f9e711deb1c82eb5a37
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-12 00:24:21 +0500

    adding assignment 18

---
+++ ASSIGNMENT NO 18/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//picking a guestlist from exercise 15
+let guestName = ['Amber Hameeed', 'Azan', 'Abdullah', 'Anus', 'Hanif', 'Zakir'];
+//printing a message
+console.log(`I am inviting ${guestName.length} people for dinner.`);
---
+++ ASSIGNMENT NO 18/main.ts
@@ -0,0 +1,10 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+//picking a guestlist from exercise 15
+
+let guestName: string[] = ['Amber Hameeed', 'Azan', 'Abdullah', 'Anus', 'Hanif', 'Zakir']
+
+//printing a message
+
+console.log(`I am inviting ${guestName.length} people for dinner.`)
