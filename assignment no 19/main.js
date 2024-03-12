decd7984b52343579213ed885a098a1a789d31bf
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-12 06:42:39 +0500

    adding assignment no 19

---
+++ assignment no 19/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let countries = ["Saudia", "Turkey", "Canada", "Italy"];
+console.log("countries I'Did like to visit :", countries);
