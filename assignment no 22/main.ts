e08847ddf0c40eaa67114dc6868e895d5f7d6719
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-14 02:05:15 +0500

    adding assignment

---
+++ assignment no 22/main.ts
@@ -0,0 +1,52 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+ let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+let car ='subaru';
+
+console.log("Is car =='subaru'? I predict True.")
+
+console.log(car =='subaru')
+
+console.log("Is car =='Toyota'? I predict False.")
+
+console.log(car =='Toyota')
+
+console.log("Is car ==='subaru'? I predict True.")
+
+console.log(car ==='subaru')
+
+console.log("Is car ==='Subaru'? I predict False.")
+
+console.log(car ==='Subaru')
+
+console.log("Is car !=='Revo'? I predict True.")
+
+console.log(car !=='Revo')
+
+console.log("Is car !=='subaru'? I predict false.")
+
+console.log(car !=='subaru')
+
+let age = 17
+
+console.log("Is age > 16? I predict True.")
+console.log(age > 16)
+
+console.log("Is age > 19? I predict False.")
+console.log(age > 19)
+
+console.log("Is age >= 17? I predict True.")
+console.log(age >= 17)
+
+console.log("Is age >= 18? I predict True.")
+console.log(age >= 18)
+
