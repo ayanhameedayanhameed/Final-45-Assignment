86488e9bcd7e33f832c8dfef77a1d96cbb4897e0
ayanhameedayanhameed <ayanhameedayanhameed4@gmail.com>
2024-03-15 01:34:09 +0500

    adding assignment 24

---
+++ assignment no 24/main.ts
@@ -0,0 +1,18 @@
+/*Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.*/
+
+//version that passes
+let alien_color = "green"
+
+if (alien_color == "green") {
+    console.log("You just earned 5 points!")
+}
+
+//version that fails (no output)
+let alienColor: string = "yellow";
+
+if (alienColor == "green") {
+    console.log("You just earned 5 points!");
+}
